9e3c03ef5b6ba5700be62750d62b70aa41376a00
sagars26 <sagar260303@gmail.com>
2023-07-21 21:13:16 +0530

    identify intent function

--- mrbot/bot.py
+++ mrbot/bot.py
@@ -5,3 +5,24 @@ import tensorflow as tf
 import numpy as np
 import nltk
 
+from nltk.stem import WordNetLemmatizer
+
+lemma = WordNetLemmatizer()
+
+intro= json.loads(open('intro.json').read())
+
+words=[]
+classes=[]
+documents=[]
+ignorel=['?','.',',','!']
+
+for intent in intro['intro']:
+    for pattern in intent['patterns']:
+        wordlist=nltk.wordpunct_tokenize(patterns)
+        words.extend(wordlist)
+        documents.append((wordlist,intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words=[lemma.lemmatize(word) for word in words if word not in ignorel]
+ 
